d3cd237eda4bc8997c3145b8cf3e9ff7b717451f
Erik Shipman <erikshipman@eriks-air.attlocal.net>
2021-02-04 15:10:32 -0500

    Committer: Erik Shipman <erikshipman@eriks-air.attlocal.net>
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	new file:   src/components/search_bar.js

---
+++ src/components/search_bar.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class SearchBar extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { term: "" };
+	}
+	render() {
+		return (
+			<div>
+				<input
+					onChange={(event) =>
+						this.setState({ term: event.target.value })
+					}
+				/>
+				Value of input : {this.state.term}
+				onChange= ...
+			</div>
+		);
+	}
+}
+
+export default SearchBar;
