587f3bf4e2d93245d22fe3215b06b61db954f65d
Erik Shipman <erikshipman@eriks-air.attlocal.net>
2021-02-04 15:22:43 -0500

    with '#' will be ignored, and an empty message aborts the commit.
     Committer: Erik Shipman <erikshipman@eriks-air.attlocal.net>
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import SearchBar from "./components/search_bar";
+import YTSearch from "youtube-api-search";
 
 const API_KEY = "AIzaSyClP5SlpB355B6l9vm3dLfh6vuEtz6DwFQ";
 
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
-};
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({ key: API_KEY, term: "surfboards" }, (videos) => {
+			// ES6 syntax for this.setState({ videos: videos});
+			this.setState({ videos });
+		});
+	}
+	render() {
+		return (
+			<div>
+				<SearchBar />
+			</div>
+		);
+	}
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
